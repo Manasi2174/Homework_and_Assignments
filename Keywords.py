8687f67fb12e6ac96c85429c4ad646547de7a558
Manasi2174 <manasijadhav2174@gmail.com>
2025-05-31 18:30:50 +0530

    Add files via upload

---
+++ Keywords.py
@@ -0,0 +1,18 @@
+# Keywords 
+# -> python keywords are some reserved words which has special meaning.
+# -> keywords cannot be used as an identifiers like variable name or function name.
+
+# In python (keyword) is built-in standard module which helps us to get full list of keywords
+
+import keyword
+
+# List of python keywords
+keywords = keyword.kwlist
+
+# Print those keywords
+print("Python Keywords : ")
+for kw in keywords:
+    print(kw)
+
+    
+
