2bc34892a92bf56d4a6c3cab3e9774896a4ce658
Manasi2174 <manasijadhav2174@gmail.com>
2025-06-03 11:54:51 +0530

    Add files via upload

---
+++ Module1.py
@@ -0,0 +1,5 @@
+# Creating the int,float and boolean variables in Module1 
+
+my_int=50
+my_float=35.78
+my_bool=True
---
+++ Module2.py
@@ -0,0 +1,6 @@
+# Accessing the variables from Module1
+
+from Module1 import my_int,my_float,my_bool
+print("The integer value is :",my_int)
+print("The boolean value is :",my_bool)
+print("the floating value is :",my_float)
