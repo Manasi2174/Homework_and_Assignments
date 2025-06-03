b04e943269a07a2a2a5772d461479a3f060db147
Manasi2174 <manasijadhav2174@gmail.com>
2025-06-03 18:44:50 +0530

    Add files via upload

---
+++ Strings_HW.py
@@ -0,0 +1,19 @@
+# Using slicing methos to print elements of string
+# Accessing the elements using negative index
+
+str="INSTAGRAM"
+
+# printing (STAG) from above string 
+print(str[-7:-3])
+
+# printing (RAM) from above string 
+print(str[-3: ])
+
+# printing (INSTA) from above string
+print(str[-9:-4])
+
+# <------------ Output----------------->
+#notes/Strings_HW.py"
+#STAG
+#RAM
+#INSTA
